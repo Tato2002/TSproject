5738caf4dde4351f4a87660a90f913c7f9189403
TkdTato <tatosarjveladzeoff@gmail.com>
2023-04-17 04:20:06 +0400

    ok

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -9,7 +9,7 @@ export class HomeComponent {
 
   imageArray = [
     {
-      imgUrl:"https://scontent.ftbs10-1.fna.fbcdn.net/v/t39.30808-6/298032058_1250484745785495_8917981470214645433_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=174925&_nc_ohc=4We8xPHTiwkAX9Q4LAs&_nc_ht=scontent.ftbs10-1.fna&oh=00_AfBlBiOckpu0pU4Mz59he-FyxKZd7BY5bmQ3lmVOGBfW8A&oe=644232B7.jpg",
+      imgUrl:"https://w.forfun.com/fetch/05/05eeb93a2e41734ecb6044146351f11e.jpeg",
       imgAlt:"idea1"
     },
     {
