82f03e0debfeed3ec4d48fcd4fc0412ef6f68299
TkdTato <tatosarjveladzeoff@gmail.com>
2023-04-17 04:12:49 +0400

    dasd

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -9,7 +9,7 @@ export class HomeComponent {
 
   imageArray = [
     {
-      imgUrl:"/assets/images/idea1.jpg",
+      imgUrl:"https://scontent.ftbs10-1.fna.fbcdn.net/v/t39.30808-6/319644229_1342975526536416_2065935949863902799_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=mIvWPxz-9hYAX8sdsXN&_nc_ht=scontent.ftbs10-1.fna&oh=00_AfAiAtz-Cxy42UIWFPD0aBS7nUEt8-A3aCWQMxpbTjcuoQ&oe=64426EB6",
       imgAlt:"idea1"
     },
     {
